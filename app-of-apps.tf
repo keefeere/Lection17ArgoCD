7c228f7e441633c9a7c41392fade98d70457fa63
Chechulin Sergey <keefeere@ukr.net>
2021-06-06 16:46:28 +0300

    app-of-apps@

--- app-of-apps.tf
+++ app-of-apps.tf
@@ -1,18 +1,19 @@
 resource "kubectl_manifest" "argo_cd_appofapps" {
     yaml_body = <<YAML
-project: default
-source:
-  repoURL: 'https://github.com/keefeere/Lection17ArgoCD.git'
-  path: apps
-  targetRevision: master
-  directory:
-    recurse: true
-    jsonnet: {}
-destination:
-  server: 'https://kubernetes.default.svc'
-  namespace: default
-syncPolicy:
-  automated:
-    prune: true
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: app-of-apps
+  namespace: argocd
+spec:
+  project: default
+  source:
+    repoURL: 'https://github.com/keefeere/Lection17ArgoCD.git'
+    targetRevision: master
+    path: apps
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: default
 YAML
 }
+
